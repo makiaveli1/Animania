b50fc3e886d9c97b76f3a25d3249510b255ce7a8
makiaveli1 <itsmakiaveli@gmail.com>
2023-09-03 12:35:45 +0100

    style the instruction popup box and setup the javascript for opening the box

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Grab the relevant elements
+    const instructionPopup = document.getElementById('instruction-popup');
+    const actionSection = document.getElementById('action-section');
+    const initiateQuizBtn = document.getElementById('initiate-quiz');
+    const closePopupBtn = document.getElementById('close-popup');
+  
+    // Function to show the popup
+    const showPopup = () => {
+      // Get the position of the action section
+      const rect = actionSection.getBoundingClientRect();
+      // Position the popup
+      instructionPopup.style.left = `${rect.left}px`;
+      instructionPopup.style.width = `${rect.width}px`;
+      instructionPopup.style.height = `${rect.height}px`;
+      // Display the popup
+      instructionPopup.style.display = 'flex';
+      instructionPopup.classList.add('show-popup');
+    };
+  
+    // Function to hide the popup
+    const hidePopup = () => {
+      instructionPopup.style.display = 'none';
+      instructionPopup.classList.remove('show-popup');
+    };
+  
+    // Show the popup when the "Instructions" button is clicked
+    initiateQuizBtn.addEventListener('click', showPopup);
+  
+    // Close the popup when the close button is clicked
+    closePopupBtn.addEventListener('click', hidePopup);
+  });
+  
