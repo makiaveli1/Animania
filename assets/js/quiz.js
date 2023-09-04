56204ef1e2acc8817d58b6e6d02990a754d5c4db
makiaveli1 <itsmakiaveli@gmail.com>
2023-09-04 09:48:28 +0100

    Style the quiz section and hide it from being displayed

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,27 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Grab the relevant elements
-    const instructionPopup = document.getElementById('instruction-popup');
-    const actionSection = document.getElementById('action-section');
-    const initiateQuizBtn = document.getElementById('initiate-quiz');
-    const closePopupBtn = document.getElementById('close-popup');
-  
-    // Function to show the popup
-    const showPopup = () => {
-      // Get the position of the action section
-      const rect = actionSection.getBoundingClientRect();
-      // Position the popup
-      instructionPopup.style.top = `${rect.top}px`;
-      instructionPopup.style.left = `${rect.left}px`;
-      instructionPopup.style.width = `${rect.width}px`;
-      instructionPopup.style.height = `${rect.height}px`;
-      // Display the popup
-      instructionPopup.style.display = 'flex';
-      instructionPopup.classList.add('show-popup');
-    };
-  
-    // Function to hide the popup
-    const hidePopup = () => {
-      instructionPopup.style.display = 'none';
-      instructionPopup.classList.remove('show-popup');
-    };
-});  
+// Function to toggle visibility of elements
+function toggleVisibility(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.toggle("hidden");
+}
+document.querySelectorAll(".category-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    toggleVisibility("category-section"); // Hide category section
+    toggleVisibility("quiz-section"); // Show quiz section
+  });
+
+  document.querySelectorAll(".category-btn").forEach((button) => {
+    button.addEventListener("click", () => {
+      toggleVisibility("category-section"); // Hide category section
+      toggleVisibility("quiz-section"); // Show quiz section
+    });
+  });
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Grab the relevant elements
-  const instructionPopup = document.getElementById('instruction-popup');
-  const actionSection = document.getElementById('action-section');
-  const initiateQuizBtn = document.getElementById('initiate-quiz');
-  const closePopupBtn = document.getElementById('close-popup');
+  const instructionPopup = document.getElementById("instruction-popup");
+  const actionSection = document.getElementById("action-section");
+  const initiateQuizBtn = document.getElementById("initiate-quiz");
+  const closePopupBtn = document.getElementById("close-popup");
 
   // Function to show the popup
   const showPopup = () => {
@@ -15,19 +15,19 @@ document.addEventListener('DOMContentLoaded', function () {
     instructionPopup.style.width = `${rect.width}px`;
     instructionPopup.style.height = `${rect.height}px`;
     // Display the popup
-    instructionPopup.style.display = 'flex';
-    instructionPopup.classList.add('show-popup');
+    instructionPopup.style.display = "flex";
+    instructionPopup.classList.add("show-popup");
   };
 
   // Function to hide the popup
   const hidePopup = () => {
-    instructionPopup.style.display = 'none';
-    instructionPopup.classList.remove('show-popup');
+    instructionPopup.style.display = "none";
+    instructionPopup.classList.remove("show-popup");
   };
 
   // Show the popup when the "Instructions" button is clicked
-  initiateQuizBtn.addEventListener('click', showPopup);
+  initiateQuizBtn.addEventListener("click", showPopup);
 
   // Close the popup when the close button is clicked
-  closePopupBtn.addEventListener('click', hidePopup);
+  closePopupBtn.addEventListener("click", hidePopup);
 });
