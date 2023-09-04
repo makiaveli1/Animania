8cb4118e6b0cdc0f4b0aafe01d368391cd5f1ee7
makiaveli1 <itsmakiaveli@gmail.com>
2023-09-04 12:37:00 +0100

    Add animations and finish up the styling of the introduction and quiz sections

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,18 +1,28 @@
 // Function to toggle visibility of elements
-function toggleVisibility(elementId) {
+function toggleVisibility(elementId, className = "hidden") {
   const element = document.getElementById(elementId);
-  element.classList.toggle("hidden");
+  element.classList.toggle(className);
 }
-document.querySelectorAll(".category-btn").forEach((button) => {
-  button.addEventListener("click", () => {
-    toggleVisibility("category-section"); // Hide category section
-    toggleVisibility("quiz-section"); // Show quiz section
-  });
 
-  document.querySelectorAll(".category-btn").forEach((button) => {
-    button.addEventListener("click", () => {
-      toggleVisibility("category-section"); // Hide category section
-      toggleVisibility("quiz-section"); // Show quiz section
-    });
-  });
+// Function to handle category button clicks
+function handleCategoryClick() {
+  const categorySection = document.getElementById("category-section");
+  const quizSection = document.getElementById("quiz-section");
+
+  // Initial setup for smooth transition
+  categorySection.style.opacity = "0";
+  quizSection.style.opacity = "0";
+  quizSection.style.display = "flex";
+
+  // Hide category section after transition
+  setTimeout(() => {
+    categorySection.style.display = "none";
+    quizSection.style.opacity = "1";
+  }, 500);  // 500ms delay to match the CSS transition
+}
+
+// Add click event listeners to the category buttons
+document.querySelectorAll(".category-btn").forEach((button) => {
+  button.addEventListener("click", handleCategoryClick);
 });
+
